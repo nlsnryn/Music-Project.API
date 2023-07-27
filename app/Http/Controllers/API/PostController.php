7ffcd5f87bb31182f630b9197c341005c3f60eb4
Nelson Ryan <nelsonarabit00@gmail.com>
2023-07-27 17:19:20 +0800

    Finished MusicApp API

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
     public function index()
     {
         try {
-            $postsPerPage = 1;
+            $postsPerPage = 3;
             $posts = Post::with('user')
                 ->orderBy('updated_at', 'desc')
                 ->simplePaginate($postsPerPage);
